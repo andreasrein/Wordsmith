874afcbac404405db28fff48acf4e6d9f0baf760
andreasrein <reinholdsson.andreas@gmail.com>
2022-11-01 23:47:16 +0100

    Add logout button

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <header></header>
-  <main>
-    <RouterView />
-  </main>
+  <div class="app-wrapper">
+    <main>
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script>
 import './styles/index.scss';
 import { RouterView } from 'vue-router';
+import Footer from '@/components/Footer.vue'
 
 export default {
-  components: { RouterView },
-  data () {
-    return {
-      loading: false
-    }
+  components: {
+    RouterView,
+    Footer
   },
   created () {
     this.$store.dispatch('auth/checkIfAuthorized')
   }
 }
 </script>
+<style lang="scss">
+  .app-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <button
+      type="button"
+      class="minor minor--green"
+      @click="handleLogout">
+      Logga ut
+    </button>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  methods: {
+    handleLogout() {
+      this.$store.commit('auth/LOGOUT')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .footer {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
