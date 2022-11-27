787885ea6affd5e9aed8f649675cb54ea137325f
andreasrein <reinholdsson.andreas@gmail.com>
2022-11-27 23:36:29 +0100

    Add proxy and remove static file

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
-import { createStore } from "vuex";
+import { createStore } from "vuex"
 import { auth } from './auth.js'
 import { sentence } from './sentence.js'
 
 export default createStore({
   state: {
-    api: 'https://ws-api.alphadev.se'
+    api: 'https://stark-earth-27607.herokuapp.com/https://ws-api.alphadev.se'
   },
   modules: {
     auth,
     sentence
   }
-});
+})
